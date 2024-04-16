4e066eee7d2001312ada7aaa1077e6b7e65d0d95
Elijah Banjo <elijahbanjo@gmail.com>
2024-04-16 16:48:14 +0100

    Feat: Added Auth to Server Endpoint

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 
+const pino = require('pino');
+
 const app = express();
 
+
 const { PORT } = require('./config/env')
+
+const Auth = require('./router/auth')
 
 //Allows us read request body
 app.use(express.json())
 
+app.use('/api/auth', Auth)
+
 app.listen(PORT, () => {
-    console.log('Server is running on port 8080');
+    console.log(`Server is running on port ${PORT}`);
 })
