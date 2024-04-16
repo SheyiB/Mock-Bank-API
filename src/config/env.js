699f966678eeb020072c16d5895adad705d29f97
Elijah Banjo <elijahbanjo@gmail.com>
2024-04-16 16:46:50 +0100

    Chore: ENV Update

--- src/config/env.js
+++ src/config/env.js
@@ -10,10 +10,12 @@ const ENV = {
     PASSWORD: process.env.SQL_PASSWORD,
     DB: process.env.DB,
     DIALECT: process.env.DIALECT,
-    POOL_MAX: process.env.POOL_MAX,
-    POOL_MIN: process.env.POOL_MIN,
+    POOL_MAX: Number(process.env.POOL_MAX),
+    POOL_MIN: Number(process.env.POOL_MIN),
     POOL_ACQUIRE: process.env.POOL_ACQUIRE,
-    POOL_IDLE: process.env.POOL_IDLE
+    POOL_IDLE: process.env.POOL_IDLE,
+    JWT_SECRET: process.env.JWT_SECRET,
+    JWT_EXPIRE: process.env.JWT_EXPIRE
 }
 
 module.exports = ENV;
