51b3fd7623ddc80d7ccfb08b9d7a201a8aa01e37
Elijah Banjo <elijahbanjo@gmail.com>
2024-04-16 15:43:21 +0100

    Chore: Database Setup

--- src/config/env.js
+++ src/config/env.js
@@ -4,7 +4,16 @@ dotenv.config();
 
 const ENV = {
     PORT: process.env.PORT,
-    PASSCODE: process.env.PASSCODE
+    PASSCODE: process.env.PASSCODE,
+    HOST: process.env.HOST,
+    USER: process.env.SQL_USER,
+    PASSWORD: process.env.SQL_PASSWORD,
+    DB: process.env.DB,
+    DIALECT: process.env.DIALECT,
+    POOL_MAX: process.env.POOL_MAX,
+    POOL_MIN: process.env.POOL_MIN,
+    POOL_ACQUIRE: process.env.POOL_ACQUIRE,
+    POOL_IDLE: process.env.POOL_IDLE
 }
 
 module.exports = ENV;
---
+++ src/model/index.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+
+const { DB, HOST, USER, PASSWORD, DIALECT, POOL_ACQUIRE, POOL_IDLE, POOL_MAX, POOL_MIN, } = require('../config/env')
+
+const sequelizeInit = new Sequelize(DB, USER, PASSWORD, {
+    host: HOST,
+    dialect: DIALECT,
+    pool: {
+        max: POOL_MAX,
+        min: POOL_MIN,
+        acquire: POOL_ACQUIRE,
+        idle: POOL_IDLE
+
+    }
+});
+
+const db = {}
+
+db.Sequelize = Sequelize;
+
+db.sequelize = sequelizeInit;
+
+db.sequelize.sync({ force: false }).then(() => {
+    console.log(`Database & tables created!`);
+})
+
+module.exports = db;
